112a89826fbfa00a92c53c5508fe4705a01f3884
Anthony Elison <tonyelison37@gmail.com>
2023-11-18 14:56:30 -0700

    Serve index html file

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-  res.write('hello, world');
-  res.end();
+  if (req.url === '/') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'index.html'),
+      (err, content) => {
+        if (err) throw err;
+
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    );
+  }
 });
 
 server.listen(PORT, () => console.log(`server running on port ${PORT}`));
