75697971605dd455b956c8482ee3daf1b0dd474b
Anthony Elison <tonyelison37@gmail.com>
2023-11-18 14:21:13 -0700

    Add basic web server with env config

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+require('dotenv').config();
+
+const { PORT } = process.env;
+
+const server = http.createServer((req, res) => {
+  res.write('hello, world');
+  res.end();
+});
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
