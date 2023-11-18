3bec21f2e733ec4ed0fac3f8ddca455944164afa
Anthony Elison <tonyelison37@gmail.com>
2023-11-18 15:55:47 -0700

    Serve html doc for the correct url or 404 page if not found

--- index.js
+++ index.js
@@ -4,19 +4,36 @@ const fs = require('fs');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 8080;
+const buildFilePath = (filename) => path.join(__dirname, 'public', filename);
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'index.html'),
-      (err, content) => {
-        if (err) throw err;
-
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(content);
+  // default content type
+  const contentType = 'text/html';
+  
+  // read the file
+  fs.readFile(
+    buildFilePath(req.url === '/' ? 'index.html' : req.url),
+    (err, content) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          // page not found
+          fs.readFile(buildFilePath('404.html'), (err, content) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content, 'utf8');
+          });
+        } else {
+          // internal server error
+          res.writeHead(500);
+          res.end(`Server Error: ${err.code}`);
+        }
+        return;
       }
-    );
-  }
+
+      // ok
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf8');
+    }
+  );
 });
 
 server.listen(PORT, () => console.log(`server running on port ${PORT}`));
